4d6c49da66e782c98cb8dac229adcc0830f568ac
Go1den <gooneden@gmail.com>
2026-05-15 10:57:59 -0500

    Gearing up for auto updates

--- forge.config.js
+++ forge.config.js
@@ -41,4 +41,17 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'Go1den',
+          name: 'PuzGod'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
 };
